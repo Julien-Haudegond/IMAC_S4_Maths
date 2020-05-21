e4eda06d296f95d045e7a3e981a8ab7646d52327
Cyrielle <lassarre.cyrielle@gmail.com>
2020-05-21 12:01:06 +0200

    Add the shark and submarine progress bar

--- scripts/index.js
+++ scripts/index.js
@@ -11,14 +11,80 @@ const computeAngle = (probs, wheelNumbers) => {
     const index = getOneEventFromProbs(probs)
     const angle = index*45 + Math.floor(5 + Math.random()*40)
 
+    dottednumber = wheelNumbers[index].innerHTML
+
     print(`Fonction computeAngle() :
         n° événement : case n°${index}
-        valeur événement : ${wheelNumbers[index].innerHTML}
+        valeur événement : ${dottednumber}
         angle normalisé calculé (45*événement + un petit "bruit" aléatoire): ${angle}°`)
     print("---------------------------")
+    //appel fonction déplacement pour faire avancer le sous-marin
+    Deplacement(dottednumber)
     return angle
 }
 
+/***** FAIRE AVANCER LE SOUS-MARIN ET LE REQUIN *****/
+
+var turn = 0
+
+elementShark = '<img id="shark" src="img/requin.jpg">'
+elementSubmarine = '<img id="submarine" src="img/sousmarin.jpg">'
+
+document.querySelector('#parcours td[data-id="1"]').innerHTML = elementShark
+document.querySelector('#parcours td[data-id="3"]').innerHTML = elementSubmarine
+
+function Deplacement(advance) {
+    shark = document.querySelector('#shark')
+    sharkId = shark.parentElement.dataset.id
+
+    submarine = document.querySelector('#submarine')
+    submarineId = submarine.parentElement.dataset.id
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      }
+
+    const go_shark = getRandomInt(3);
+    const go_submarine = parseInt(advance);
+
+    sum_shark = parseInt(sharkId)+go_shark
+    sum_submarine = parseInt(submarineId)+go_submarine
+
+    console.log(sum_shark)
+    console.log(sum_submarine)
+
+    if (turn<=5){
+        if(sum_shark==sum_submarine){
+            alert("Game over")
+        }
+        else if(sum_submarine>sum_shark){
+            //pour vider la case dans laquelle les images se trouvaient
+            if(go_shark != 0){
+                document.querySelector('#parcours td[data-id="'+sharkId+'"]').innerHTML=''
+            }
+
+            if(go_submarine != 0){
+                document.querySelector('#parcours td[data-id="'+submarineId+'"]').innerHTML=''
+            }
+
+            //pour replacer les images dans les nouvelles bonnes cases
+            if(go_shark != 0){
+                document.querySelector('#parcours td[data-id="'+sum_shark+'"]').innerHTML = elementShark
+            }
+
+            if(go_submarine != 0){
+                document.querySelector('#parcours td[data-id="'+sum_submarine+'"]').innerHTML = elementSubmarine
+            }
+            turn++
+        }
+        else{
+            alert("Le requin va trop vite !")
+        }
+    }
+    else{
+        alert("End of the game, see you soon")
+    }
+}
 
 /*
  ************
@@ -73,3 +139,4 @@ checkbox.addEventListener('click', () => {
 slider.addEventListener('input', () => {
     probFromSlider = getProbFromSlider(slider)
 })
+
